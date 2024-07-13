e2ce3a124e269f8844d9406dcb461571bb595e00
HaderCabrera <hader.cabrera96@gmail.com>
2024-07-13 04:50:08 -0500

    Modificando entity/servicios

--- aeroline/src/main/java/com/aeroline/Main.java
+++ aeroline/src/main/java/com/aeroline/Main.java
@@ -1,7 +1,16 @@
 package com.aeroline;
 
+import user.application.UserUseCase;
+import user.domain.service.UserService;
+import user.infraestructure.inController.UserController;
+import user.infraestructure.outRepository.UserRepository;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserService userService = new UserRepository();
+        UserUseCase userUseCase = new UserUseCase(userService);
+        UserController userController = new UserController(userUseCase);
+        userController.start();
+      
     }
 }
