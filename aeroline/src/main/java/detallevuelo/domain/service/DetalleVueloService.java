0f94f16f6f333fa5db4a6db4aa6b9ef7364d3d9e
juandavidg0mez <nihlgomez@gamil.com>
2024-07-14 19:10:39 -0500

    Estudio Hexagonal creacion de application detalleVuelo

--- aeroline/src/main/java/detallevuelo/domain/service/DetalleVueloService.java
+++ aeroline/src/main/java/detallevuelo/domain/service/DetalleVueloService.java
@@ -3,10 +3,10 @@ package detallevuelo.domain.service;
 import detallevuelo.domain.entity.DetalleVuelo;
 
 public interface DetalleVueloService {
-    void consultarDetalleVuelo();
-    DetalleVuelo eliminarDetalleVuelo(String numero_vuelo);
+    DetalleVuelo consultarDetalleVuelo(String id_vuelo);
+    void eliminarDetalleVuelo(String numero_vuelo);
     DetalleVuelo actualizarDetallevulo();
-    void  consultarInfoTripulacion(String id_empleado);
+    DetalleVuelo consultarInfoTripulacion(String id_empleado);
     DetalleVuelo editarEscalaVuelo();
     
 }
