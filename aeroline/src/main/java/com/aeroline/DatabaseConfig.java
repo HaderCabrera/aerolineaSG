06dc6d15716fff4aaf9d85ac916074b5a76372e0
juandavidg0mez <nihlgomez@gmail.com>
2024-07-18 05:41:10 -0500

    Actualizacion y uso de caso

--- aeroline/src/main/java/com/aeroline/DatabaseConfig.java
+++ aeroline/src/main/java/com/aeroline/DatabaseConfig.java
@@ -4,36 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    // COMANDO PARA QUITAR EL COMENTARIO : ctrl + k + u
-
-
-    // RAILWAY 
-
-
-
-
-    private static final String URL = "jdbc:mysql://roundhouse.proxy.rlwy.net:41600/railway";
-    private static final String USER = "root";
-    private static final String PASSWORD = "FtjNcJYULtcbURPVLBrmAReMgZcdLjAK";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
     
-   
-
-    // MYSQL 
-
-/*
-  private static final String URL = "jdbc:mysql://127.0.0.1:3306/railway";
-    private static final String USER = "campus2023";
-    private static final String PASSWORD = "campus2023";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
- 
- */
 private static final String URL = "jdbc:mysql://roundhouse.proxy.rlwy.net:41600/railway";
 private static final String USER = "root";
 private static final String PASSWORD = "FtjNcJYULtcbURPVLBrmAReMgZcdLjAK";
@@ -42,13 +13,7 @@ public static Connection getConnection() throws SQLException {
     return DriverManager.getConnection(URL, USER, PASSWORD);
 }
 
-    // private static final String URL = "jjdbc:mysql://127.0.0.1:3306/railway";
-    // private static final String USER = "campus2023";
-    // private static final String PASSWORD = "campus2023";
-
-    // public static Connection getConnection() throws SQLException {
-    //     return DriverManager.getConnection(URL, USER, PASSWORD);
-    // }
+  
 
 
 
